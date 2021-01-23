6e850b5db44e250ce6a39bfe1aeb4b703448287a
Yu Lin <gremlinweekend@yus-macbook-air.myfiosgateway.com>
2021-01-23 14:32:08 -0500

    configured about and preveious layouts

--- app/src/main/java/com/gremlinweekend/numad21s_yulin/FirstFragment.java
+++ app/src/main/java/com/gremlinweekend/numad21s_yulin/FirstFragment.java
@@ -23,11 +23,9 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.button_about).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                System.out.print("Hello World");
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
